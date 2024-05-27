3e28315d3cbd4f4c466bfd402cb2ae2c583dc2f2
Joshua Park <50765702+JoshParkSJ@users.noreply.github.com>
2024-05-26 19:52:15 -0700

    demo specific

--- browser.py
+++ browser.py
@@ -15,13 +15,12 @@ from playwright.sync_api._generated import Playwright as SyncPlaywright, Page, B
 from playwright.sync_api import expect
 from langchain_openai import AzureChatOpenAI
 
-token = "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjJGNUQxNzI3NEQ3NjREQzlERENGNDRBOEI3NzE5QUY2NjlCRjc4RTAiLCJ4NXQiOiJMMTBYSjAxMlRjbmR6MFNvdDNHYTltbV9lT0EiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FscGhhLnVpcGF0aC5jb20vaWRlbnRpdHlfIiwibmJmIjoxNzE2NzU5MTc3LCJpYXQiOjE3MTY3NTk0NzcsImV4cCI6MTcxNjc2MzA3NywiYXVkIjpbIk9yY2hlc3RyYXRvckFwaVVzZXJBY2Nlc3MiLCJNYW5hZ2VMaWNlbnNlIiwiT01TIiwiQWlGYWJyaWMiLCJCdXNpbmVzc1VzZXJQb3J0YWxQcm94eUFwaSIsIkRhdGFTZXJ2aWNlQXBpVXNlckFjY2VzcyIsIkNvbm5lY3Rpb25TZXJ2aWNlIiwiQ29ubmVjdGlvblNlcnZpY2VVc2VyIiwiSmFtSmFtQXBpIiwiSW5zaWdodHMiLCJJbnNpZ2h0cy5JbnRlZ3JhdGlvbnMiLCJQcm9jZXNzTWluaW5nIiwiVGFza01pbmluZyIsIlNlcnZlcmxlc3NDb250cm9sUGxhbmUiLCJJZGVudGl0eVNlcnZlckFwaSIsIkRvY3VtZW50VW5kZXJzdGFuZGluZyIsIkRvY3VtZW50VW5kZXJzdGFuZGluZ1MyUyIsIlVpUGF0aC5PcmNoZXN0cmF0b3IiLCJTdHVkaW9XZWJCYWNrZW5kIiwiU3R1ZGlvV2ViVHlwZUNhY2hlU2VydmljZSIsIkN1c3RvbWVyUG9ydGFsIiwiQXVkaXQiLCJVaVBhdGguRG9jdW1lbnRVbmRlcnN0YW5kaW5nIiwiQXV0b21hdGlvblNvbHV0aW9ucyIsIlJlaW5mZXIiLCJSZXNvdXJjZUNhdGFsb2dTZXJ2aWNlQXBpIiwiU2VhcmNoUmVjb21tZW5kYXRpb25zU2VydmljZSIsIkluc2lnaHRzLlJlYWxUaW1lRGF0YSIsIkdsb2JhbENsaWVudE1hbmFnZW1lbnQuSW50ZXJuYWwiLCJBY2FkZW15Il0sInNjb3BlIjpbIkFjYWRlbXkiLCJBaUZhYnJpYyIsIkF1ZGl0LlJlYWQiLCJBdXRvbWF0aW9uU29sdXRpb25zIiwiQnVzaW5lc3NVc2VyUG9ydGFsUHJveHlBcGkiLCJDb25uZWN0aW9uU2VydmljZSIsIkNvbm5lY3Rpb25TZXJ2aWNlVXNlciIsIkN1c3RvbWVyUG9ydGFsIiwiRGF0YVNlcnZpY2VBcGlVc2VyQWNjZXNzIiwiRGlyZWN0b3J5IiwiRG9jdW1lbnRVbmRlcnN0YW5kaW5nIiwiRHUuQWlQcm94eSIsIkR1LkNsYXNzaWZpY2F0aW9uLkFwaSIsIkR1LkRpZ2l0aXphdGlvbi5BcGkiLCJEdS5FeHRyYWN0aW9uLkFwaSIsIkR1Lk1ldGVyaW5nIiwiRHUuU3RvcmFnZS5QcmVzaWduZWRVcmwiLCJEdS5UcmFpbmluZy5TZXJ2aWNlIiwiRHUuVmFsaWRhdGlvbi5BcGkiLCJlbWFpbCIsIkdsb2JhbENsaWVudE1hbmFnZW1lbnQuSW50ZXJuYWwiLCJJZGVudGl0eVNlcnZlckFwaSIsIkluc2lnaHRzIiwiSW5zaWdodHMuSW50ZWdyYXRpb25zIiwiSW5zaWdodHMuUmVhbFRpbWVEYXRhIiwiSmFtSmFtQXBpIiwiTWFuYWdlTGljZW5zZSIsIk9NUyIsIm9wZW5pZCIsIk9SLkFkbWluaXN0cmF0aW9uLlJlYWQiLCJPcmNoZXN0cmF0b3JBcGlVc2VyQWNjZXNzIiwiUHJvY2Vzc01pbmluZyIsInByb2ZpbGUiLCJSQ1MuRm9sZGVyQXV0aG9yaXphdGlvbiIsIlJDUy5UYWdzTWFuYWdlbWVudCIsIlJlZmVyZW5jZVRva2VuIiwiUmVpbmZlciIsIlNDUC5Kb2JzLlJlYWQiLCJTQ1AuUnVudGltZXMiLCJTQ1AuUnVudGltZXMuUmVhZCIsIlNSUy5FdmVudHMiLCJTUlMuUmVjb21tZW5kYXRpb25zIiwiU3R1ZGlvV2ViQmFja2VuZCIsIlN0dWRpb1dlYlR5cGVDYWNoZVNlcnZpY2UiLCJUYXNrTWluaW5nIiwib2ZmbGluZV9hY2Nlc3MiXSwiYW1yIjpbImV4dGVybmFsIl0sInN1Yl90eXBlIjoidXNlciIsImNsaWVudF9pZCI6IjExMTlhOTI3LTEwYWItNDU0My1iZDFhLWFkNmJmYmJjMjdmNCIsInN1YiI6ImZkMjI0ZWViLTZlOTUtNDNlNS1hOGIwLTM2MWQzYjViYWYzYSIsImF1dGhfdGltZSI6MTcxNjc0ODgwOCwiaWRwIjoib2lkYyIsImVtYWlsIjoiam9zaHVhLnBhcmtAdWlwYXRoLmNvbSIsIkFzcE5ldC5JZGVudGl0eS5TZWN1cml0eVN0YW1wIjoiQ0dVSVhXRFUzWVNYN0w0NDdQMzVSRzdLRUI2WEFUQ1giLCJhdXRoMF9jb24iOiJnb29nbGUtb2F1dGgyIiwiY291bnRyeSI6IiIsImV4dF9zdWIiOiJnb29nbGUtb2F1dGgyfDEwNTA4NDU0MTE4MTUwNjU0OTk2MyIsIm1hcmtldGluZ0NvbmRpdGlvbkFjY2VwdGVkIjoiRmFsc2UiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jTDJGTU9EcTk1T2lic1hsbFdwamZRSXhJLUQwWndUUV92TVpiNFhYSGhKTmc4enVnPXM5Ni1jIiwicHJ0X2lkIjoiOTU2OGJlYmEtNTBhOC00OWQxLTgwMWUtZjJkMTcxMTA4OWZkIiwiaG9zdCI6IkZhbHNlIiwiZmlyc3RfbmFtZSI6Ikpvc2giLCJsYXN0X25hbWUiOiIiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicHJlZmVycmVkX3VzZXJuYW1lIjoiam9zaHVhLnBhcmtAdWlwYXRoLmNvbSIsIm5hbWUiOiJqb3NodWEucGFya0B1aXBhdGguY29tIiwiZXh0X2lkcF9pZCI6IjEiLCJleHRfaWRwX2Rpc3BfbmFtZSI6Ikdsb2JhbElkcCIsInNpZCI6IjhFRTRFQUUyNDU1MDUxMDlCMkUzRUYyQTM4RDE4ODcxIiwianRpIjoiNjQxODAzOUM3NDIyNUM4MkJCMjhGQTNDQzk2RDlGQjAifQ.LtIjJ355wbwUeLH1YRnpr_2_Go8LnlKKQMCZFwVO9NzSrDsOMs_e9rvwSwKUuWpazxHVCfYzKIJGv_I0xDePwXR4OZ935wzXerzB-AUEmuzWNe09ilTt5GagqHLmcXmOeezNnU2QKf3V6scHg9uaViDOgDKuJZJo4RZPewzWjK3HcWcEmTiP4MfcK9DolGTcu9il0r8T_e9foNhetWOrkODG0DxetC7AA1rZ2eAPL0sUQ0zrw6a-jgPZMh_x3t3Ivo2WtzEyO10-yB6h9EDal95yTiD_3aPynOyhr8dK-bjADSJtjI6LycgCRrz6XqxlYV6oDKiZLhZx3zxsg_qfEA"
+token = "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjJGNUQxNzI3NEQ3NjREQzlERENGNDRBOEI3NzE5QUY2NjlCRjc4RTAiLCJ4NXQiOiJMMTBYSjAxMlRjbmR6MFNvdDNHYTltbV9lT0EiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FscGhhLnVpcGF0aC5jb20vaWRlbnRpdHlfIiwibmJmIjoxNzE2NzY5ODU2LCJpYXQiOjE3MTY3NzAxNTYsImV4cCI6MTcxNjc3Mzc1NiwiYXVkIjpbIk9yY2hlc3RyYXRvckFwaVVzZXJBY2Nlc3MiLCJNYW5hZ2VMaWNlbnNlIiwiT01TIiwiQWlGYWJyaWMiLCJCdXNpbmVzc1VzZXJQb3J0YWxQcm94eUFwaSIsIkRhdGFTZXJ2aWNlQXBpVXNlckFjY2VzcyIsIkNvbm5lY3Rpb25TZXJ2aWNlIiwiQ29ubmVjdGlvblNlcnZpY2VVc2VyIiwiSmFtSmFtQXBpIiwiSW5zaWdodHMiLCJJbnNpZ2h0cy5JbnRlZ3JhdGlvbnMiLCJQcm9jZXNzTWluaW5nIiwiVGFza01pbmluZyIsIlNlcnZlcmxlc3NDb250cm9sUGxhbmUiLCJJZGVudGl0eVNlcnZlckFwaSIsIkRvY3VtZW50VW5kZXJzdGFuZGluZyIsIkRvY3VtZW50VW5kZXJzdGFuZGluZ1MyUyIsIlVpUGF0aC5PcmNoZXN0cmF0b3IiLCJTdHVkaW9XZWJCYWNrZW5kIiwiU3R1ZGlvV2ViVHlwZUNhY2hlU2VydmljZSIsIkN1c3RvbWVyUG9ydGFsIiwiQXVkaXQiLCJVaVBhdGguRG9jdW1lbnRVbmRlcnN0YW5kaW5nIiwiQXV0b21hdGlvblNvbHV0aW9ucyIsIlJlaW5mZXIiLCJSZXNvdXJjZUNhdGFsb2dTZXJ2aWNlQXBpIiwiU2VhcmNoUmVjb21tZW5kYXRpb25zU2VydmljZSIsIkluc2lnaHRzLlJlYWxUaW1lRGF0YSIsIkdsb2JhbENsaWVudE1hbmFnZW1lbnQuSW50ZXJuYWwiLCJBY2FkZW15Il0sInNjb3BlIjpbIkFjYWRlbXkiLCJBaUZhYnJpYyIsIkF1ZGl0LlJlYWQiLCJBdXRvbWF0aW9uU29sdXRpb25zIiwiQnVzaW5lc3NVc2VyUG9ydGFsUHJveHlBcGkiLCJDb25uZWN0aW9uU2VydmljZSIsIkNvbm5lY3Rpb25TZXJ2aWNlVXNlciIsIkN1c3RvbWVyUG9ydGFsIiwiRGF0YVNlcnZpY2VBcGlVc2VyQWNjZXNzIiwiRGlyZWN0b3J5IiwiRG9jdW1lbnRVbmRlcnN0YW5kaW5nIiwiRHUuQWlQcm94eSIsIkR1LkNsYXNzaWZpY2F0aW9uLkFwaSIsIkR1LkRpZ2l0aXphdGlvbi5BcGkiLCJEdS5FeHRyYWN0aW9uLkFwaSIsIkR1Lk1ldGVyaW5nIiwiRHUuU3RvcmFnZS5QcmVzaWduZWRVcmwiLCJEdS5UcmFpbmluZy5TZXJ2aWNlIiwiRHUuVmFsaWRhdGlvbi5BcGkiLCJlbWFpbCIsIkdsb2JhbENsaWVudE1hbmFnZW1lbnQuSW50ZXJuYWwiLCJJZGVudGl0eVNlcnZlckFwaSIsIkluc2lnaHRzIiwiSW5zaWdodHMuSW50ZWdyYXRpb25zIiwiSW5zaWdodHMuUmVhbFRpbWVEYXRhIiwiSmFtSmFtQXBpIiwiTWFuYWdlTGljZW5zZSIsIk9NUyIsIm9wZW5pZCIsIk9SLkFkbWluaXN0cmF0aW9uLlJlYWQiLCJPcmNoZXN0cmF0b3JBcGlVc2VyQWNjZXNzIiwiUHJvY2Vzc01pbmluZyIsInByb2ZpbGUiLCJSQ1MuRm9sZGVyQXV0aG9yaXphdGlvbiIsIlJDUy5UYWdzTWFuYWdlbWVudCIsIlJlZmVyZW5jZVRva2VuIiwiUmVpbmZlciIsIlNDUC5Kb2JzLlJlYWQiLCJTQ1AuUnVudGltZXMiLCJTQ1AuUnVudGltZXMuUmVhZCIsIlNSUy5FdmVudHMiLCJTUlMuUmVjb21tZW5kYXRpb25zIiwiU3R1ZGlvV2ViQmFja2VuZCIsIlN0dWRpb1dlYlR5cGVDYWNoZVNlcnZpY2UiLCJUYXNrTWluaW5nIiwib2ZmbGluZV9hY2Nlc3MiXSwiYW1yIjpbImV4dGVybmFsIl0sInN1Yl90eXBlIjoidXNlciIsImNsaWVudF9pZCI6IjExMTlhOTI3LTEwYWItNDU0My1iZDFhLWFkNmJmYmJjMjdmNCIsInN1YiI6ImZkMjI0ZWViLTZlOTUtNDNlNS1hOGIwLTM2MWQzYjViYWYzYSIsImF1dGhfdGltZSI6MTcxNjc0ODgwOCwiaWRwIjoib2lkYyIsImVtYWlsIjoiam9zaHVhLnBhcmtAdWlwYXRoLmNvbSIsIkFzcE5ldC5JZGVudGl0eS5TZWN1cml0eVN0YW1wIjoiQ0dVSVhXRFUzWVNYN0w0NDdQMzVSRzdLRUI2WEFUQ1giLCJhdXRoMF9jb24iOiJnb29nbGUtb2F1dGgyIiwiY291bnRyeSI6IiIsImV4dF9zdWIiOiJnb29nbGUtb2F1dGgyfDEwNTA4NDU0MTE4MTUwNjU0OTk2MyIsIm1hcmtldGluZ0NvbmRpdGlvbkFjY2VwdGVkIjoiRmFsc2UiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jTDJGTU9EcTk1T2lic1hsbFdwamZRSXhJLUQwWndUUV92TVpiNFhYSGhKTmc4enVnPXM5Ni1jIiwicHJ0X2lkIjoiOTU2OGJlYmEtNTBhOC00OWQxLTgwMWUtZjJkMTcxMTA4OWZkIiwiaG9zdCI6IkZhbHNlIiwiZmlyc3RfbmFtZSI6Ikpvc2giLCJsYXN0X25hbWUiOiIiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicHJlZmVycmVkX3VzZXJuYW1lIjoiam9zaHVhLnBhcmtAdWlwYXRoLmNvbSIsIm5hbWUiOiJqb3NodWEucGFya0B1aXBhdGguY29tIiwiZXh0X2lkcF9pZCI6IjEiLCJleHRfaWRwX2Rpc3BfbmFtZSI6Ikdsb2JhbElkcCIsInNpZCI6IjhFRTRFQUUyNDU1MDUxMDlCMkUzRUYyQTM4RDE4ODcxIiwianRpIjoiRDgxOTc5NjVCRTU0QjY2NEU1QzNBRDRGM0RFQjExMTIifQ.WL8aS-R4A-ynkeiJMai3946uaZxl7NtTBQI372MNa8c3etv1kAUr7CQZVdiCCMPpiyleYdgWmdfCXKyt3r9XWMrz1LMuXng0A9KcW63riEn5Y9osm0WBhqtMNlddrEzdXqQHtKdj3jj6RV7eyoBBLTjGt6gKTbzYU0DCQJAZZIM_5MuGs9U8VtJuD3zrEpXwCEKzwkfmFIQArSNoFkMAkWtbVwKorluOqGpMdp2PKBmPA_miv9K04ETNI5YHcDgBgnBjPNyRYPWR25nt25-TZbNVgFtYcrTDKp_XCR0ZFa1oNTnr6RSjLY5jujM72JVt2ouurOdHU-AGI21As4FYAA"
 
 def get_llm():
     return AzureChatOpenAI(
-        # deployment_name="gpt-4-32k",
+        deployment_name="gpt-4-32k",
         # deployment_name="gpt-4-vision-preview",
-        deployment_name="gpt-4",
         openai_api_version="2023-06-01-preview",
         azure_endpoint="https://alpha.uipath.com/dcfa21f1-a818-4fab-a05d-4fd4292c9ccd/bdd155c1-6ae5-4088-acdd-4cb6bb58e267/llmgateway_/", # Standard Url routing(org/tenant) for LLM Gateway. You do not need to specify a resource name, as the LLM Gateway will route the request to the correct resource.
         openai_api_type="azure",
@@ -100,6 +99,7 @@ class PageContext:
     #     self.context.close()
     #     self.browser.close()
 
+# ui-tree-item is for demo only
 chat_history = [
     {
         "role": "system",
@@ -109,35 +109,58 @@ Select 1 action from the <action> list that you think will accomplish the user's
 
 Here are the only actions you can use:
 <actions>
-    <action key="ClickButton" description="clicks a button with a specific id" parameters=[targetTag] />
+    <action key="Click" description="clicks on a specific <button>, <div>, <ui-tree-item>, or <input> tag" parameters=[targetTag] />
     <action key="FillInputField" description="fills in an input field with a specific id" parameters=[targetTag, value] />
     <action key="GoToUrl" description="Navigates to URL" parameters=[url] />
     <action key="GoBack" description="Navigates to previously visited URL" />
+    <action key="PressEscapeKey" description="Presses escape key on keyboard" />
+    <action key="Test" description="Tests workflow done so far" />
     <action key="Done" description="Indicates user their request is completed" />
 </actions>
 
 Here are examples of valid complete responses with example data from different chat conversations: 
-[ClickButton][targetTag: button[id='myButtonId']]
-[ClickButton][targetTag: button[aria-label='Add an activity']]
-[ClickButton][targetTag: button[class='button-class-123']]
+[Click][targetTag: button[id='myButtonId']]
+[Click][targetTag: button[aria-label='Add an activity']]
+[Click][targetTag: ui-tree-item[id='tree-item']]
+[Click][targetTag: input[aria-label='Click to use a variable']]
+[Click][targetTag: div[class='div-class-123']]
 [FillInputField][targetTag: input[aria-label='Search for an activity']][value: "asana"]
 [FillInputField][targetTag: input[id='activity-searchbar-123']][value: "activity name"]
 [FillInputField][targetTag: input[placeholder='Search term...']][value: "microsoft"]
+[FillInputField][targetTag: div[contenteditable="true"]][value: ""CAD""]
 [GoToUrl][url: "https://www.google.com"]
 [GoToUrl][url: "https://www.linkedin.com"]
+[PressEscapeKey]
+[Test]
 [GoBack]
 [Done]
 
-You must format the output exactly in the format specified above. DO NOT UNDER ANY CIRCUMSTANCES, MAKE UP A HTML TAG
+You must format the output exactly in the format specified above.
+
+DO NOT UNDER ANY CIRCUMSTANCES, MAKE UP A HTML TAG
         """
     },
 ]
 
-def isRelevantAttrValue(x):
-    return 0 < len(x) < 50 and not x[0].isdigit()
+# def isRelevantTag(key, val):
+#     return 0 < len(val) < 50 and not val[0].isdigit() and 0 < len(key) < 20
 
-def isRelevantAttrKey(x):
-    return 0 < len(x) < 20
+# for uipath studio (demo only)
+def isRelevantTag(key, val, name):
+    if name == "div" and key != "contenteditable":
+        # only read contenteditable divs
+        return False
+    if not (0 < len(val) < 50 and not val[0].isdigit()):
+        return False
+    if not (0 < len(key) < 20):
+        return False
+    if key == "aria-label" and val in ["Actions", "See more"]:
+        return False
+    if key == "data-cy" and val in ["manual-trigger-add-argument", "dictionary-open", "remove-button"]:
+        return False
+    if key == "data-cy" and "sw-cp-package-UiPath" in val:
+        return False
+    return True
 
 def prune_html(browser):
     # general purpose
@@ -146,10 +169,9 @@ def prune_html(browser):
     # for uipath studio (demo only)
     html_content = browser.page.query_selector('mat-drawer-content.mat-drawer-content')
     html_content = html_content.inner_html()
-
     if (browser.page.query_selector('.mdc-dialog__container')): # if popup exists, show popup only
         html_content = browser.page.query_selector('.mdc-dialog__container').inner_html()
-    if (browser.page.query_selector('.mat-mdc-dialog-surface')): # if dropdown exists, show dropdown only
+    if (browser.page.query_selector('.mat-mdc-dialog-surface')): # if dropdown exists, append it to view (and truncate the background html since it's less relevant and expensive)
         html_content = browser.page.query_selector('.mat-mdc-dialog-surface').inner_html()
 
     soup = BeautifulSoup(html_content, 'html.parser')
@@ -157,34 +179,43 @@ def prune_html(browser):
 
     def construct_new_tag(orig_tag):
         new_tag = soup.new_tag(orig_tag.name)
+
         for k, v in orig_tag.attrs.items():
-            if isRelevantAttrKey(k) and isRelevantAttrValue(v):
-                # If value is a list (i.e class="msg-overlay artdeco-button artdeco-button...") only take the first element (i.e class="msg-overlay")
+            if isRelevantTag(k, v, orig_tag.name):
                 value = v
                 if isinstance(v, list):
+                    # If value is a list (i.e class="msg-overlay artdeco-button artdeco-button...") 
+                    # only take the first element (i.e class="msg-overlay")
                     value = v[0]
                 new_tag.attrs[k] = value
         for child in orig_tag:
-            if child.name:
+            if child.name and child.name != "ui-package-icon": # for uipath studio only ui-package-icon (demo only)
                 # html tag children
                 child_new = construct_new_tag(child)
                 new_tag.append(child_new)
             else:
                 # non-html tags, just text
-                new_tag.append(str(child))
+                if len(str(child)) < 50:
+                    new_tag.append(str(child))
         return new_tag
 
-    for tag in soup.find_all(["button", "input", "a"]):
+    for tag in soup.find_all(["button", "input", "a", "ui-input-widget", "ui-tree-item"]): # for uipath studio only (demo only) - ui-tree-item, ui-input-widget
         new_tag = construct_new_tag(tag)
+
         if new_tag.attrs or new_tag.contents:
             simplifiedHTML += str(new_tag.prettify()) + "\n"
     
+    # for uipath studio only (demo only) - keep hitting into api quota
+    if len(simplifiedHTML) > 15000:
+        if len(simplifiedHTML) > 25000:
+            simplifiedHTML = simplifiedHTML[:len(simplifiedHTML)//6]
+        else:
+            simplifiedHTML = simplifiedHTML[:len(simplifiedHTML)//3]
+
     return simplifiedHTML
 
 
 def parse_and_execute_action(browser, response):
-    print("response: ", response)
-
     # parses [<action>][<targetTag>: <target>][<value key>: "<value>"]
     action_search = re.search(r'\[(\w+)\]', response)
     action = action_search.group(1) if action_search else ""
@@ -192,15 +223,36 @@ def parse_and_execute_action(browser, response):
     target_search = re.search(r'targetTag\: (.*?\])', response)
     target = target_search.group(1) if target_search else ""
 
-    value_search = re.search(r'value\: "(.*?)"', response)
+    value_search = re.search(r'value\: "(\"?.*?\"?)"', response)
     value = value_search.group(1) if value_search else ""
 
     url_search = re.search(r'url\: "(.*?)"', response)
     url = url_search.group(1) if url_search else ""
 
+    print("response: ", response)
     print("parsed action: ", action, target, value, url)
-    if (action == "ClickButton"):
+    if (action == "Click"):
         browser.page.click(target)
+
+        # for demo only - @studio team, why are you not removing this popup after it is closed??!!?!
+        if "Set Variable Value" in target:
+            browser.page.evaluate('''() => {
+                let elem = document.querySelector('div.mdc-dialog__container');
+                if (elem) {
+                    elem.parentNode.removeChild(elem);
+                }
+            }''')
+            time.sleep(1)
+        if "Currency" in target:
+            browser.page.evaluate('''() => {
+                let elem = document.querySelector('div.mat-mdc-dialog-surface');
+                if (elem) {
+                    elem.parentNode.removeChild(elem);
+                }
+            }''')
+            time.sleep(1)
+
+        time.sleep(0.5)
 
     if (action == "FillInputField"):
         browser.page.fill(target, value)
@@ -215,8 +267,14 @@ def parse_and_execute_action(browser, response):
     if (action == "PressEnterKey"):
         browser.page.keyboard.press('Enter')
 
-    if (action == "PressEscKey"):
-        browser.page.keyboard.press("Escape")
+    if (action == "PressEscapeKey"):
+        # browser.page.keyboard.press("Escape")
+
+        # for uipath studio only (demo only)
+        browser.page.click("ui-ribbon[class='ng-star-inserted']")
+        
+    if (action == "Test"): # for uipath studio only (demo only)
+        browser.page.click("button[aria-describedby='cdk-describedby-message-ng-1-122']")
 
     if (action == "Done"):
         global current_iteration
@@ -230,12 +288,16 @@ def start_agent(browser, model):
     # prune HTML content
     simplifiedHTML = prune_html(browser)
 
+    # debugging purposes
+    with open(str(current_iteration) + 'output.txt', 'w') as f:
+        f.write(simplifiedHTML)
+
     # show agent HTML
     response = model.invoke([
         *chat_history,
         {
             "role": "system",
-            "content": """You are in URL {url}. Select the next appropriate action to accomplish the user's goal. 
+            "content": """You are in URL {url}. Select the next appropriate action to accomplish the user's goal. DO NOT UNDER ANY CIRCUMSTANCES, MAKE UP A HTML TAG
 
             Here is a simplified view of the website HTML you are currently in:
             {html}""".format(url=browser.page.url, html=simplifiedHTML)
@@ -263,15 +325,8 @@ def start_agent(browser, model):
     #     }
     # )
 
-    # debugging purposes
-    with open(str(current_iteration) + 'output.txt', 'w') as f:
-        f.write(simplifiedHTML)
-
     # execute action
     parse_and_execute_action(browser, response.content)
-
-    # optional; let the page load
-    time.sleep(0.5)
 
     # update agent on progress
     chat_history.append({
@@ -279,7 +334,6 @@ def start_agent(browser, model):
         "content": "Status update: you have triggerd an action {response}. Do not repeat the same action twice in a row.".format(response=response.content)
     })
 
-    print("current_iteration: ", current_iteration)
     current_iteration += 1
     if (current_iteration < MAX_ITERATION):
         start_agent(browser, model)
@@ -296,7 +350,8 @@ if __name__ == '__main__':
             "content": user_request
         })
 
+        print("\n")
+
         model = get_llm()
         
-        start_agent(browser, model)
         start_agent(browser, model)
